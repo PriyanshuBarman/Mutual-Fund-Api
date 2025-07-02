accb47a0cbe112a5ce8e1871e57a3c83f158d292
Priyanshu Barman <priyanshubarman741@gmail.com>
2025-07-02 17:03:02 +0530

    save the data to db

--- server.js
+++ server.js
@@ -1,15 +1,7 @@
 import express from "express";
-import run from "./src/controllers/test.controller.js";
+import "./src/scripts/seedMfData.js";
 
 const app = express();
-
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-
-app.use("/ping", async (req, res) => {
-  const data = await run();
-  res.status(200).json({ message: "ok" });
-});
 
 app.listen(process.env.PORT || 3000, () => {
   console.log("Server is running");
