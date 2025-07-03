0f6ad262f5b75ed0e28e322f8bfdf14fb955df5d
Priyanshu Barman <priyanshubarman741@gmail.com>
2025-07-03 19:36:58 +0530

    add cors

--- server.js
+++ server.js
@@ -1,12 +1,20 @@
+import "dotenv/config";
 import express from "express";
 import { router } from "./src/routes/router.js";
-// import "./src/scripts/seedMfData.js";
+import cors from "cors";
 
 const app = express();
+
+app.use(cors({ origin: process.env.FRONTEND_URL || "http://localhost:5173", credentials: true }));
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.use("/api/v1", router);
+
+app.get("/", (req, res) => {
+  res.send("Welcome to Envest Helper API");
+});
 
 app.listen(process.env.PORT || 3000, () => {
   console.log("Server is running");
